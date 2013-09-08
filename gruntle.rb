8d1fc9754834c56f25818859421fe17b9f51dabb
Bill Weiss <bill@backstopsolutions.com>
2013-09-07 20:26:35 -0500

    Added TweetMachine for streaming.  Seems to work?
    Hard to test until I get a loop waiting for it

--- gruntle.rb
+++ gruntle.rb
@@ -2,13 +2,21 @@ require 'YAML'
 require 'Twitter'
 require 'tweetstream'
 
-config = YAML.load(File.open('config.yaml'))
+@config = YAML.load(File.open('config.yaml'))
 
 Twitter.configure do |twconfig|
-    twconfig.consumer_key = config['consumer_key']
-    twconfig.consumer_secret = config['consumer_secret']
-    twconfig.oauth_token = config['access_token']
-    twconfig.oauth_token_secret = config['access_token_secret']
+    twconfig.consumer_key = @config['consumer_key']
+    twconfig.consumer_secret = @config['consumer_secret']
+    twconfig.oauth_token = @config['access_token']
+    twconfig.oauth_token_secret = @config['access_token_secret']
+end
+
+TweetStream.configure do |twconfig|
+    twconfig.consumer_key = @config['consumer_key']
+    twconfig.consumer_secret = @config['consumer_secret']
+    twconfig.oauth_token = @config['access_token']
+    twconfig.oauth_token_secret = @config['access_token_secret']
+    twconfig.auth_method = :oauth
 end
 
 begin
@@ -24,9 +32,19 @@ end
 
 def handle_dm ( dm )
     if @readtweets.include? dm.id
-        # nothing, it's cool
+        # we've already seen this one, don't do anything with it
     else
-        Twitter.update(dm.text)
+        if @config['validusers'].include? dm.sender.screen_name
+            puts "New DM from #{dm.sender.screen_name}, ID #{dm.id}"
+            Twitter.update(dm.text)
+            @readtweets << dm.id
+
+            # yup, write it out every time.  Whatever, it's low volume
+            File.open(Dir.pwd + '/seen.yaml', 'w+') {|f| f.write(@readtweets.to_yaml) }
+        else
+            puts "#{dm.sender.screen_name} sent a DM, won't rebroadcast"
+        end
+
         @readtweets << dm.id
 
         # yup, write it out every time.  Whatever, it's low volume
@@ -38,3 +56,10 @@ Twitter.direct_messages.each do |dm|
     handle_dm (dm)
 end
 
+@client = TweetStream::Client.new
+@client.on_direct_message do |dm|
+    handle_dm dm
+end
+@client.on_error do |message|
+    puts "Error!  #{message}"
+end
