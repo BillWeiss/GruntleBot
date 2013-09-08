3c7de14142070e09a4dce414a276fe8b593c85ea
Bill Weiss <bill@backstopsolutions.com>
2013-09-07 21:40:42 -0500

    Starting on the anti-spam measures

--- gruntle.rb
+++ gruntle.rb
@@ -2,6 +2,8 @@ require 'YAML'
 require 'Twitter'
 require 'tweetstream'
 
+NO_AT_MESSAGE = "Sorry, I won't broadcast anything with an @ in it as a spam safeguard"
+
 @config = YAML.load(File.open('config.yaml'))
 
 Twitter.configure do |twconfig|
@@ -36,6 +38,10 @@ def handle_dm ( dm )
     else
         if @config['validusers'].include? dm.sender.screen_name
             puts "New DM from #{dm.sender.screen_name}, ID #{dm.id}"
+
+            if dm.text.includes? '@'
+                Twitter.direct_message_create(dm.sender, NO_AT_MESSAGE)
+            end
             Twitter.update(dm.text)
         else
             puts "#{dm.sender.screen_name} sent a DM, won't rebroadcast"
